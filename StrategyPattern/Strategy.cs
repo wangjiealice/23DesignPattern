9c39e28d4430b410594055be4800e52329afeb46
Wang, Alice <alice.wang@zeiss.com>
2021-02-07 17:43:14 +0800

    Add strategy pattern.

---
+++ StrategyPattern/Strategy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPattern
+{
+    class Algorithm
+    {
+        IStrategy algorithmStrategy;
+        public Algorithm(IStrategy strategy)
+        {
+            algorithmStrategy = strategy;
+        }
+
+        public int ExecuteStrate(int num1, int num2)
+        {
+            return algorithmStrategy.DoOperation(num1, num2);
+        }
+    }
+}
